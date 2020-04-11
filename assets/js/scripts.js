ca8c98e9b05196de45af55fe8c71194c1d19b2af
Émerson Henrique <emerson.eho@gmail.com>
2020-04-11 12:28:37 +0100

    Criação de preloader

---
+++ assets/js/scripts.js
@@ -0,0 +1,9 @@
+//<![CDATA[
+$(window).on('load', function () {
+    $('#preloader .inner').fadeOut();
+    $('#preloader').delay(350).fadeOut('slow');
+    $('body').delay(350).css({
+        'overflow': 'visible'
+    });
+})
+//]]>
