ed3b992e36322c192c50648190c89f989c7bcb5f
Émerson Oliveira <emerson.oliveira@nka.pt>
2020-06-12 18:39:23 +0100

    Change mobile menu

--- assets/js/mobile-menu.js
+++ assets/js/mobile-menu.js
@@ -1,11 +1,16 @@
 'use strict';
 
+const element = document.getElementsByClassName('mobile-menu')[0];
+
 //Open mobile menu
-$('.menu__mobile-button, .mobile-menu__close').on('click', function() {
-    $('.mobile-menu').toggleClass('active');
+
+$('.menu__mobile-button, .mobile-menu__close').on('click', function () {
+    //$('.mobile-menu').toggleClass('active');
+    element.classList.toggle("active");
 });
 
 //Close mobile menu after click
-$('.mobile-menu__wrapper ul li a').on('click', function() {
-    $('.mobile-menu').removeClass('active');
+$('.mobile-menu__wrapper ul li a').on('click', function () {
+    element.classList.remove('active');
+    //$('.mobile-menu').removeClass('active');
 });
